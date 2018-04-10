ed68502eae582be2985a3fbadb93e3906d7ca8cb
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-04-10 21:32:18 +0700

    migrations: courses

---
+++ database/migrations/2018_04_10_141312_create_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('author_id');
+            $table->string('title', 60);
+            $table->string('slug', 80);
+            $table->text('description');
+            $table->string('thumbnail');
+            $table->float('rating');
+            $table->unsignedInteger('student_count');
+            $table->unsignedDecimal('price');
+            $table->unsignedTinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
