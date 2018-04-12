7d8822be99b4028d9eef5008fdbd44bc6e4b37e5
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-04-12 21:38:59 +0700

    refactor: gunakan fungsi-fungsi bawaan Eloquent untuk menyimpan data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,15 +24,21 @@ class DatabaseSeeder extends Seeder
         foreach ($users as $user) {
             $userCourses = factory(\App\Course::class)
                 ->times(rand(0, 3))
-                ->create([
-                     'author_id' => $user->id,
-                     'category_id' => $categories->random()->id,
-                 ]);
+                ->make();
+
+            // asosiasikan course dengan category dan author
+            // setiap course punya beberapa video
+            foreach($userCourses as $course) {
+                $course->author()->associate($user);
+                $course->category()->associate($categories->random()->getKey());
+                $course->save();
+            }
 
             // setiap course punya beberapa video
             foreach($userCourses as $course) {
                 foreach (range(1, 5) as $index) {
-                    factory(\App\Video::class)->create(['course_id' => $course->id, 'order' => $index]);
+                    $video = factory(\App\Video::class)->make(['order' => $index]);
+                    $course->videos()->save($video);
                 }
                 $courses[] = $course;
             }
@@ -43,7 +49,7 @@ class DatabaseSeeder extends Seeder
         foreach($users as $user)
         {
             // @todo: tidak boleh menambahkan course punya sendiri ke wishlist
-            $user->wishlists()->sync([$courses->random()->id, $courses->random()->id]);
+            $user->wishlists()->sync([$courses->random()->getKey(), $courses->random()->getKey()]);
         }
 
         // Enrolled Course
