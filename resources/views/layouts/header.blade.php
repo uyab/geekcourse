f488c8c21e44f696c762b9d03debf2a345d844ec
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-04-19 17:08:19 +0700

    top menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,22 @@
 <nav class="navbar border-bottom {{ $headerClass ?? '' }}">
     <a class="navbar-brand" href="#">GeekCourse</a>
     @auth
+
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Manage Course</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">My Wishlist</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">My Enrolled Course</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">My Profile Page</a>
+            </li>
+        </ul>
+
         <form action="{{ route('logout') }}" method="POST">
             @csrf
             <button type="submit" class="btn btn-outline-primary">Logout</button>
