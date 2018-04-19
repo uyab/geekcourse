da0fe2f06b7b8ef268f6778e3b74e34ac01306d2
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-04-19 16:55:26 +0700

    styling header and modify redirect after login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,11 @@
-<nav class="navbar navbar-light {{ $headerClass ?? '' }}">
+<nav class="navbar border-bottom {{ $headerClass ?? '' }}">
     <a class="navbar-brand" href="#">GeekCourse</a>
+    @auth
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-outline-primary">Logout</button>
+        </form>
+    @else
+        <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+    @endauth
 </nav>
